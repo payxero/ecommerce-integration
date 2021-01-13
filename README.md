99685bb6c176f71841aadc43e3e856624b6ac199
errodriguez <emilio.ruiz@elasticdevteam.com>
2021-01-13 18:32:06 -0500

    Update payment request

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ecommerce-integration
-This repo demonstrates how to connect to the API from an angular application
+This repo demonstrates how to connect to the API from an angular application.
+Please download ecommerce-integration-api to run correctly.
--- src/app/core/services/payment.service.ts
+++ src/app/core/services/payment.service.ts
@@ -6,60 +6,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PaymentService {
 
-    url: string;
+    apiUrl: string;
 
     constructor(
         private readonly http: HttpClient,
 
     ) {
-        this.url = 'https://api.zerospay.com/v1/public/payment';
+        this.apiUrl = 'http://localhost:3000/payment/charge';
     }
 
     submitPayment(charge: any): Promise<any> {
-        const data = {
-            query: `mutation charge(
-                $clientId: ID!,
-                $token: String,
-                $amount: Float!,
-                $surcharge: Float!,
-                $currency: String,
-                $card: CardInput!,
-                $contact: ContactInput!,
-                $billingAddress: AddressInput!,
-                $shippingAddress: AddressInput!,
-                $order: OrderInput!,
-                $sendReceipt: Boolean,
-                $capture: Boolean!,
-                $paymentRequestId: String,
-                $percent: Float,
-                $from: String,
-                $fromId: String
-              ){
-                charge(chargeData:{
-                clientId: $clientId,
-                token: $token,
-                amount: $amount,
-                surcharge: $surcharge,
-                currency: $currency,
-                card: $card,
-                contact: $contact,
-                billingAddress: $billingAddress,
-                shippingAddress: $shippingAddress,
-                order: $order,
-                sendReceipt: $sendReceipt,
-                capture: $capture,
-                paymentRequestId: $paymentRequestId,
-                percent: $percent,
-                from: $from,
-                fromId: $fromId
-              }){
-                transactionId
-              }
-            }`,
-            variables: charge
-        };
         return new Promise((resolve, reject) => {
-            this.http.post(this.url, data).subscribe(resolve, reject);
+            this.http.post(this.apiUrl, charge).subscribe(resolve, reject);
         });
     }
 
